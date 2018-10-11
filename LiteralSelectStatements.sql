e0524f369a668d4d43e9e8d8d09f69d3d8eea6e4
anthonyg0715 <anthony_2457@hotmail.com>
2018-10-11 08:42:47 -0400

    LiteralSelectStatementsPracticeProblems

---
+++ LiteralSelectStatements.sql
@@ -0,0 +1,5 @@
+/*Using the literal select statement which returns as is literals meaning no rows or columns
+In the included pdf you will see the problem questions and the results
+*/
+select 'Anthony Garzon',7*4,(7-4) * 8, 'Saratoga Hospitals SQL Training Class',
+'Day 1 of Training', 5*3
---
+++ LiteralSelectStatements/LiteralSelectStatements.sql
@@ -0,0 +1,3 @@
+/*Using the literal select statement which returns as is literals meaning no rows or columns*/
+select 'Anthony Garzon',7*4,(7-4) * 8, 'Saratoga Hospitals SQL Training Class',
+'Day 1 of Training', 5*3
